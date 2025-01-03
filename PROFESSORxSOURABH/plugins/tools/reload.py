dbea22553c66ed24eb9c4e7014cd4cc76481cb7d
Why You Visit My Profile 😂 <153857610+prf100rabhAaBb40769374divyanshi40769374@users.noreply.github.com>
2025-01-03 12:38:57 +0530

    [ 𝗣𝗥𝚯𝗙𝗘𝗦𝗦𝚯𝗥 ][ ѕαиαтαиι ]

--- PROFESSORxSOURABH/plugins/tools/reload.py
+++ PROFESSORxSOURABH/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7355202884)
+    & filters.user(7400823450)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
@@ -118,7 +118,7 @@ async def help(client: Client, message: Message):
              [
                  [
                       InlineKeyboardButton(
-                         "• 𝖧ᴀᴄᴋᴇᴅ вʏ  •", url=f"https://t.me/venompratapchat")
+                         "• 𝖧ᴀᴄᴋᴇᴅ вʏ  •", url=f"https://t.me/Professor_modz")
                  ]
             ]
          ),
